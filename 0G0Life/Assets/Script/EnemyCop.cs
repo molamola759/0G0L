b433274126e9ce3cd4fdc5f4b2de3ee378457706
molamola759 <mrs7592001@gmail.com>
2021-12-24 16:02:35 +0800

    Add enemy detect system

--- 0G0Life/Assets/Script/EnemyCop.cs
+++ 0G0Life/Assets/Script/EnemyCop.cs
@@ -8,7 +8,7 @@ public class EnemyCop : MonoBehaviour
     #region
     [Header("CheckTheChasingArea")]
     public Vector3 v3TrackSize = Vector3.one;
-    public Vector3 v3TrackOffset;
+    public Vector3 v3Trackoffset;
 
     [Header("EnemyCopHP"), Range(0, 20)]
     public int EnemyCop_HP = 10;
@@ -27,7 +27,78 @@ public class EnemyCop : MonoBehaviour
 
     [Header("TargetLayer")]
     public LayerMask layerTarget;
+
+    [Header("Animation")]
+    public string parameterWalk = "Run";
+
+    [Header("FacingTargetObj")]
+    public Transform target;
+
+    private float angle = 0;
+    private Rigidbody2D rig;
+    private Animator ani;
     #endregion
 
-    
+    #region
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.3f);
+        Gizmos.DrawCube(transform.position + transform.TransformDirection(v3Trackoffset), v3TrackSize);
+    }
+    #endregion
+
+    private void Update()
+    {
+        CheckTargetInArea();
+    }
+
+    #region
+    ///<summary>
+    ///Check the target is in the area or not
+    ///</summary>
+
+    private void CheckTargetInArea()
+    {
+        // 2D physic.CoverBox(Central, Size, Angle)
+        Collider2D hit = Physics2D.OverlapBox(transform.position + transform.TransformDirection(v3Trackoffset), v3TrackSize, 0, layerTarget);
+
+        //if (hit) print(hit.name);
+        //if (hit) rig.velocity = new Vector2(-EnemyCop_MoveSpeed, rig.velocity.y);
+
+        if (hit) Move();
+    }
+    #endregion
+
+    private void Move()
+    {
+        //if target X < Enemy X means at the left angle 0
+        //if target X > Enemy X means at the right angle 180
+
+        if (target.position.x > transform.position.x)
+        {
+            //right angle = 180
+        }
+        else if (target.position.x < transform.position.x)
+        {
+            //Left angle = 0
+        }
+        // ternary operator: bool? If bool is true : If bool is false;
+        angle = target.position.x > transform.position.x ? 180 : 0;
+
+        transform.eulerAngles = Vector3.up * angle;
+        //rig.velocity = new Vector2(-EnemyCop_MoveSpeed, rig.velocity.y);
+        rig.velocity = transform.TransformDirection(new Vector2(-EnemyCop_MoveSpeed, rig.velocity.y));
+        ani.SetBool(parameterWalk, true);
+
+        // Distance = 3 dimensional vector.Distance(point A&point B)
+        float distance = Vector3.Distance(target.position, transform.position);
+        print("Distance With the Target:" + distance);
+    }
+
 }
